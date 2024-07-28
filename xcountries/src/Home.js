3d7a11d6c3f8d4f56d852f7b204a6c17dbb4c926
Kamlesh Savant <sawantk45@gmail.com>
2024-07-28 14:43:37 +0530

    class name re-swiped

--- xcountries/src/Home.js
+++ xcountries/src/Home.js
@@ -2,68 +2,60 @@ import React, { useEffect, useState } from 'react';
 import './Home.css';
 
 function Home() {
+  const [countryList, setCountryList] = useState([]);
+  const [filterCountryList, setFilterCountryList] = useState([]);
+  const [search, setSearch] = useState('');
 
-    const [countryList, setCountryList] = useState([]);
-    const [filterCountryList, setFilterCountryList] = useState([]);
-    const [search, setSearch] = useState('');
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        let response = await fetch('https://restcountries.com/v3.1/all');
+        let data = await response.json();
+        setCountryList(data);
+        setFilterCountryList(data);  // Initialize the filterCountryList with the full data set
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    fetchCountry();
+  }, []);
 
-
+  useEffect(() => {
     const filterCountry = () => {
+      const newCountryList = countryList.filter(country =>
+        country.name.common.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilterCountryList(newCountryList);
+    };
 
-        const newCountryList = countryList.filter( country => country.name.common.toLowerCase().includes(search.toLowerCase()));
-        setFilterCountryList(newCountryList);
-    }
+    filterCountry();
+  }, [search, countryList]);
 
-    const fetchCountry = async() => {
-        try {
-
-            let response = await fetch('https://restcountries.com/v3.1/all');
-            let data = await response.json();
-            setCountryList(data);
-            // console.log(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    useEffect(()=>{
-        fetchCountry();
-    },[]);
-    useEffect(()=>{
-        filterCountry();
-    },[search]);
+  const renderCountryCards = (countries) => {
+    return countries.map(country => (
+      <div className='countryCard' key={country.name.common}>
+        <img src={country.flags.png} alt={country.name.common} />
+        <h2>{country.name.common}</h2>
+      </div>
+    ));
+  };
 
   return (
     <>
-    <div className='search-box'>
-        <input type="text" placeholder='Search for countries...' onChange={(e)=>setSearch(e.target.value)} value={search}/>
-    </div>
-    <div className='container'>
-
-        {search === "" ? countryList.map(
-            (country)=>{
-                return (
-                    <div className='countryCard' key={country.name.common}>
-                        <img src={country.flags.png} alt={country.name.common} />
-                        <h2>{country.name.common}</h2>
-                    </div>
-                )
-            }
-        ) :  filterCountryList.map(
-            (country)=>{
-                return (
-                    <div className='countryCard' key={country.name.common}>
-                        <img src={country.flags.png} alt={country.name.common} />
-                        <h2>{country.name.common}</h2>
-                    </div>
-                )
-            }
-        )}
-        
-    </div>
+      <div className='search-box'>
+        <input
+          type="text"
+          placeholder='Search for countries...'
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+      </div>
+      <div className='container'>
+        {renderCountryCards(search === "" ? countryList : filterCountryList)}
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
